5b41ec8cf996c7f69a803c17441282a1fe87423b
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-11-15 19:12:00 +0000

    Add stave to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
 import Keyboard from "./component/Keyboard";
+import Stave from "./component/Stave";
 
 function App() {
+
+    // So a note is displayed on the stave
+    // If you click the right note on the keyboard then:
+    // * the note goes green
+    // * the note plays correctly
+    // * a random new note appears
+    // However if its incorrect the note:
+    // * the note goes red
+    // * an incorrect buzzer noise is played
+
     return (
         <>
+            <Stave/>
             <Keyboard/>
         </>
     );
---
+++ src/component/Stave.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Stave: React.FC = () => {
+    return (
+        <div className="staff-container">
+            <div className="staff">
+                <div className="bar double"/>
+                <div className="clef treble"/>
+                <div className="quarter note f5"/>
+                <div className="bar end"/>
+            </div>
+        </div>
+    )
+}
+
+export default Stave;
+
+
+
+
+
+
+
+
