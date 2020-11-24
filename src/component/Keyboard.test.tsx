1db219ffa73afed7b0217051fe3b4c7a12c6f024
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-11-24 21:19:27 +0000

    Increase test coverage for keyboard

--- src/component/Keyboard.test.tsx
+++ src/component/Keyboard.test.tsx
@@ -9,3 +9,14 @@ test('should contain 22 keys', () => {
     expect(linkElement).toBeInTheDocument();
     expect(linkElement.children.length).toBe(22)
 });
+
+test('should execute function when note played', async () => {
+    const mockFunction = jest.fn(() => {});
+    const result = render(<Keyboard onNotePlayed={mockFunction}/>);
+
+    const key = result.getByTestId("key-c5-white");
+    await key.click()
+
+    expect(mockFunction.mock.calls.length).toBe(1);
+});
+
