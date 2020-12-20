31e4e9d6963bb80a8f972b666f8541dbe4f46d38
nathancashmore <nathan.cashmore@staticvoid.co.uk>
2020-12-20 11:22:04 +0000

    Add toggle to hint text

--- src/__tests__/Hint.test.tsx
+++ src/__tests__/Hint.test.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import Hint from '../component/Hint';
 
 describe("Hint", () => {
     test('renders hint before assessment', () => {
-        render(<Hint expectedNote={"c5"} notePlayed={undefined}/>)
-        expect(screen.getByTestId("hint-default-note-message")).toBeInTheDocument()
+        render(<Hint expectedNote={"c5"} notePlayed={undefined} />, )
+        clickToggle()
         expect(screen.getByTestId("hint-expected-note")).toHaveTextContent("c5")
     });
 
     test('renders hint after expected note has been played', () => {
         render(<Hint expectedNote={"c5"} notePlayed={"c5"}/>)
-        expect(screen.getByTestId("hint-default-note-message")).toBeInTheDocument()
+        clickToggle()
         expect(screen.getByTestId("hint-expected-note")).toHaveTextContent("c5")
     });
 
     test('renders hint after wrong note has been played', () => {
         render(<Hint expectedNote={"c5"} notePlayed={"c6"}/>)
+        clickToggle()
         expect(screen.getByTestId("hint-bad-note-message")).toBeInTheDocument()
         expect(screen.getByTestId("hint-expected-note")).toHaveTextContent("c5")
         expect(screen.getByTestId("hint-bad-note-message")).toHaveTextContent("c6")
     });
+
+    test('hint not displayed by default', async () => {
+        render(<Hint expectedNote={"c5"} notePlayed={undefined}/>)
+        expect(screen.queryByTestId("hint-expected-note")).toBeNull()
+    })
+
+    function clickToggle() {
+        fireEvent.click(screen.getByTestId('hint-toggle').firstChild as HTMLInputElement)
+    }
 })
